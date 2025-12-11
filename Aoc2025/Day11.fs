342b101d16d763dcc42e4b78c6755d8dc0ba097b
Jeff Williams <jeffw@wherethebitsroam.com>
2025-12-11 22:17:37 +0100

    Save

--- Aoc2025/Day11.fs
+++ Aoc2025/Day11.fs
@@ -16,36 +16,42 @@ module Parser =
         | Success(result, _, _) -> result
         | Failure(error, _, _) -> failwith error
 
-let rec follow (m: Map<string, list<string>>) acc paths =
-    printfn "%A %A" (List.length acc) (List.length paths)
+let follow (m: Map<string, list<string>>) (src: string) (dst: string) =
+    let rec follow' acc paths =
+        printfn "%A %A" (List.length acc) (List.length paths)
 
-    match paths with
-    | [] -> acc
-    | paths ->
-        let nexts =
-            paths
-            |> List.collect (fun path ->
-                let curr = List.head path
+        match paths with
+        | [] -> acc
+        | paths ->
+            let nexts =
+                paths
+                |> List.collect (fun path ->
+                    let curr = List.head path
 
-                let nextDevices =
-                    match m |> Map.tryFind curr with
-                    | Some x -> x
-                    | None -> failwithf $"Not found `{curr}`"
+                    if curr = "out" then
+                        []
+                    else
+                        let nextDevices =
+                            match m |> Map.tryFind curr with
+                            | Some x -> x
+                            | None -> failwithf $"Not found `{curr}`"
 
-                nextDevices |> List.map (fun next -> next :: path))
+                        nextDevices |> List.map (fun next -> next :: path))
 
-        let complete, rest = nexts |> List.partition (fun path -> List.head path = "out")
+            let complete, rest = nexts |> List.partition (fun path -> List.head path = dst)
 
-        follow m (List.append acc complete) rest
+            follow' (List.append acc complete) rest
+
+    follow' [] [ [ src ] ]
 
 let part1 (s: string) =
     let m = s |> Parser.parse |> Map.ofList
-    follow m [] [ [ "you" ] ] |> List.length
+    follow m "you" "out" |> List.length
 
 let part2 (s: string) =
     let m = s |> Parser.parse |> Map.ofList
 
-    follow m [] [ [ "svr" ] ]
-    // |> List.filter (fun path -> path |> List.contains "dac")
-    // |> List.filter (fun path -> path |> List.contains "fft")
-    |> List.length
+    follow m "fft" "dac"
+// |> List.filter (fun path -> path |> List.contains "dac")
+// |> List.filter (fun path -> path |> List.contains "fft")
+// |> List.length
